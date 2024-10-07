a4eef00c9e3f47dfcd4fcc9366180a0df31b607b
mapleok <142672414+mapleok@users.noreply.github.com>
2024-10-07 14:09:01 +0800

    Add files via upload

---
+++ exp1/vector.cpp
@@ -0,0 +1,205 @@
+typedef int Rank;
+#define DEFAULT_CAPACITY 3
+
+template <typename T>
+class Vector
+{
+protected:
+    Rank _size;
+    int _capacity;
+    T *_elem;
+    void copyFrom(T const *A, Rank lo, Rank hi);
+    void expand();
+    void shrink();
+    bool bubble(Rank lo, Rank hi);
+    void bubbleSort(Rank lo, Rank hi);
+    Rank max(Rank lo, Rank hi);
+    void selectionSort(Rank lo, Rank hi);
+    void merge(Rank lo, Rank hi);
+    void mergeSort(Rank lo, Rank hi);
+    Rank partition(Rank lo, Rank hi);
+    void quickSort(Rank lo, Rank hi);
+    void heapSort(Rank lo, Rank hi);
+
+public:
+    Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0)
+    {
+        _elem = new T[_capacity = c];
+        for (_size = 0; _size < s; _elem[_size++] = v)
+            ;
+    }
+    Vector(T const *A, Rank n)
+    {
+        copyFrom(A, 0, n);
+    }
+    Vector(T const *A, Rank lo, Rank hi)
+    {
+        copyFrom(A, lo, hi);
+    }
+    Vector(Vector<T> const &V) { copyFrom(V._elem, 0, V._size); }
+    Vector(Vector<T> const &V, Rank lo, Rank hi) { copyFrom(V._elem, lo, hi); }
+    // 析构函数
+    ~Vector() { delete[] _elem; }
+    // 只读访问接口
+    Rank size() const { return _size; }
+    bool empty() const { return !_size; }
+    int disordered() const;
+    Rank find(T const &e) const { return find(e, 0, _size); }
+    Rank find(T const &e, Rank lo, Rank hi) const;
+    Rank search(T const &e) const
+    {
+        return (0 > _size) ? -1 : search(e, 0, _size);
+    }
+    // 可写访问接口
+    T &operator[](Rank r) const;
+    Vector<T> &operator=(Vector<T> const &);
+    T remove(Rank r);
+    int remove(Rank lo, Rank hi);
+    Rank insert(Rank r, T const &e);
+    Rank insert(T const &e) { return insert(_size, e); }
+    void sort(Rank lo, Rank hi);
+    void sort() { sort(0, _size); }
+    void unsort(Rank lo, Rank hi);
+    void unsort() { unsort(0, _size); }
+    int deduplicate();
+    int uniquify();
+
+    // 遍历
+    void traverse(void (*)(T &));
+    template <typename VST>
+    void traverse(VST &);
+};
+
+//基于复制的构造方法
+template <typename T>
+void Vector<T>::copyFrom(T const*A,Rank lo,Rank hi){
+    _elem = new T[_capacity = 2 * (hi - lo)];
+    _size = 0;
+    while(lo<hi)
+        _elem[_size++] = A[lo++];
+}
+
+
+//扩容
+template <typename T>
+void Vector<T>::expand()
+{
+    if (_size < _capacity)
+        return;
+    if(_capacity<DEFAULT_CAPACITY)
+        _capacity = DEFAULT_CAPACITY;
+    T *oldElem = _elem;
+    _elem=new T[_capacity<<=1];
+    for (int i = 0; i < _size; i++)
+    {
+        _elem[i] = oldElem[i];
+    }
+    delete[] oldElem;
+}
+
+//缩容
+template<typename T>void Vector<T>::shrink(){
+    if(_capacity<DEFAULT_CAPACITY<<1)
+        return;
+    if(_size<<2>_capacity)
+        return;
+    T *oldElem = _elem;
+    _elem = new T[_capacity >>= 1];
+    for (int i = 0; i < _size;i++)
+        _elem[i] = oldElem[i];
+    delete[] oldElem;
+}
+
+//直接引用元素
+template <typename T>
+T &Vector<T>::operator[](Rank r) const
+{
+    return _elem[r]; // 返回元素
+}
+
+//置乱算法
+template<typename T>void permute(Vector<T>&V){
+    for (int i = V.size(); i > 0;i--)
+        swap(V[i - 1], V[rand() % i]);
+}
+//区间置乱算法
+template<typename T>void Vector<T>::unsort(Rank lo,Rank hi){
+    T *V = _elem + lo;
+    for (Rank i = hi - lo; i > 0;i--)
+        swap(V[i - 1], V[rand() % i]);
+}
+
+//顺序查找
+template<typename T>
+Rank Vector<T>::find(T const&e,Rank lo,Rank hi)const{
+    while((lo<hi--)&&(e!=_elem[hi]))
+        ;
+    return hi;
+}
+
+//插入
+template <typename T>
+Rank Vector<T>::insert(Rank r, T const &e)
+{
+    expand();
+    for (int i = _size; i > r; --i)
+    {
+        _elem[i] = _elem[i - 1]; // 移动元素
+    }
+    _elem[r] = e; // 插入新元素
+    _size++;
+    return r;
+}
+
+
+//区间删除remove（lo,hi)
+template<typename T>int Vector<T>::remove(Rank lo,Rank hi){
+    if(lo==hi)
+        return 0;
+    while(hi<size)
+        _elem[lo++] = _elem[hi++];
+    _size = lo;
+    shrink();
+    return hi - lo;
+}
+//单个元素
+template <typename T>
+T Vector<T>::remove(Rank r)
+{
+    T e = _elem[r]; // 备份被删除的元素
+    remove(r, r + 1);//调用区间删除算法，等效于对[r，r+1)的删除
+    return e;
+}
+
+//遍历
+template <typename T>
+void Vector<T>::traverse(void (*visit)(T &))
+{
+    for (int i = 0; i < _size; ++i)
+    {
+        visit(_elem[i]); // 调用访问函数
+    }
+}
+
+template<typename T>template<typename VST>//原宿类型，操作器
+void Vector<T>::traverse(VST& visit)
+{
+    for (int i = 0; i < size;i++)
+        visit(_elem[i]);
+}
+
+//唯一化
+template<typename T>int Vector<T>::uniquify(){
+//低效    // int oldSize = _size;
+    // int i;
+    // while(i<_size)
+    //     _elem[i - 1] == _elem[i] ? remove(i) : i++;
+    // return oldSize - _size;
+    Rank i = 0,j = 0;
+    while(++j<_size)
+        if(_elem[i]!=_elem[j])
+            _elem[++i] = _elem[j];
+    _size = ++i;
+    shrink();
+    return j - i;
+}
