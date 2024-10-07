9e610f562d3131d0d463a9bd786cf6e4ca8a9011
mapleok <142672414+mapleok@users.noreply.github.com>
2024-10-07 16:22:39 +0800

    Update vector.cpp

--- exp1/vector.cpp
+++ exp1/vector.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+using namespace std;
 typedef int Rank;
 #define DEFAULT_CAPACITY 3
 
@@ -12,16 +19,18 @@ protected:
     void expand();
     void shrink();
     bool bubble(Rank lo, Rank hi);
-    void bubbleSort(Rank lo, Rank hi);
+
     Rank max(Rank lo, Rank hi);
     void selectionSort(Rank lo, Rank hi);
-    void merge(Rank lo, Rank hi);
-    void mergeSort(Rank lo, Rank hi);
+    void merge(Rank lo, Rank mi, Rank hi);
+
     Rank partition(Rank lo, Rank hi);
     void quickSort(Rank lo, Rank hi);
     void heapSort(Rank lo, Rank hi);
 
 public:
+    void bubbleSort(Rank lo, Rank hi);
+    void mergeSort(Rank lo, Rank hi);
     Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0)
     {
         _elem = new T[_capacity = c];
@@ -70,26 +79,26 @@ public:
     void traverse(VST &);
 };
 
-//基于复制的构造方法
+// 基于复制的构造方法
 template <typename T>
-void Vector<T>::copyFrom(T const*A,Rank lo,Rank hi){
+void Vector<T>::copyFrom(T const *A, Rank lo, Rank hi)
+{
     _elem = new T[_capacity = 2 * (hi - lo)];
     _size = 0;
-    while(lo<hi)
+    while (lo < hi)
         _elem[_size++] = A[lo++];
 }
 
-
-//扩容
+// 扩容
 template <typename T>
 void Vector<T>::expand()
 {
     if (_size < _capacity)
         return;
-    if(_capacity<DEFAULT_CAPACITY)
+    if (_capacity < DEFAULT_CAPACITY)
         _capacity = DEFAULT_CAPACITY;
     T *oldElem = _elem;
-    _elem=new T[_capacity<<=1];
+    _elem = new T[_capacity <<= 1];
     for (int i = 0; i < _size; i++)
     {
         _elem[i] = oldElem[i];
@@ -97,47 +106,54 @@ void Vector<T>::expand()
     delete[] oldElem;
 }
 
-//缩容
-template<typename T>void Vector<T>::shrink(){
-    if(_capacity<DEFAULT_CAPACITY<<1)
+// 缩容
+template <typename T>
+void Vector<T>::shrink()
+{
+    if (_capacity < DEFAULT_CAPACITY << 1)
         return;
-    if(_size<<2>_capacity)
+    if (_size << 2 > _capacity)
         return;
     T *oldElem = _elem;
     _elem = new T[_capacity >>= 1];
-    for (int i = 0; i < _size;i++)
+    for (int i = 0; i < _size; i++)
         _elem[i] = oldElem[i];
     delete[] oldElem;
 }
 
-//直接引用元素
+// 直接引用元素
 template <typename T>
 T &Vector<T>::operator[](Rank r) const
 {
     return _elem[r]; // 返回元素
 }
 
-//置乱算法
-template<typename T>void permute(Vector<T>&V){
-    for (int i = V.size(); i > 0;i--)
+// 置乱算法
+template <typename T>
+void permute(Vector<T> &V)
+{
+    for (int i = V.size(); i > 0; i--)
         swap(V[i - 1], V[rand() % i]);
 }
-//区间置乱算法
-template<typename T>void Vector<T>::unsort(Rank lo,Rank hi){
+// 区间置乱算法
+template <typename T>
+void Vector<T>::unsort(Rank lo, Rank hi)
+{
     T *V = _elem + lo;
-    for (Rank i = hi - lo; i > 0;i--)
-        swap(V[i - 1], V[rand() % i]);
+    for (Rank i = hi - lo; i > 0; i--)
+        std::swap(V[i - 1], V[rand() % i]);
 }
 
-//顺序查找
-template<typename T>
-Rank Vector<T>::find(T const&e,Rank lo,Rank hi)const{
-    while((lo<hi--)&&(e!=_elem[hi]))
+// 顺序查找
+template <typename T>
+Rank Vector<T>::find(T const &e, Rank lo, Rank hi) const
+{
+    while ((lo < hi--) && (e != _elem[hi]))
         ;
     return hi;
 }
 
-//插入
+// 插入
 template <typename T>
 Rank Vector<T>::insert(Rank r, T const &e)
 {
@@ -151,27 +167,28 @@ Rank Vector<T>::insert(Rank r, T const &e)
     return r;
 }
 
-
-//区间删除remove（lo,hi)
-template<typename T>int Vector<T>::remove(Rank lo,Rank hi){
-    if(lo==hi)
+// 区间删除remove（lo,hi)
+template <typename T>
+int Vector<T>::remove(Rank lo, Rank hi)
+{
+    if (lo == hi)
         return 0;
-    while(hi<size)
+    while (hi < size())
         _elem[lo++] = _elem[hi++];
     _size = lo;
     shrink();
     return hi - lo;
 }
-//单个元素
+// 单个元素
 template <typename T>
 T Vector<T>::remove(Rank r)
 {
-    T e = _elem[r]; // 备份被删除的元素
-    remove(r, r + 1);//调用区间删除算法，等效于对[r，r+1)的删除
+    T e = _elem[r];   // 备份被删除的元素
+    remove(r, r + 1); // 调用区间删除算法，等效于对[r，r+1)的删除
     return e;
 }
 
-//遍历
+// 遍历
 template <typename T>
 void Vector<T>::traverse(void (*visit)(T &))
 {
@@ -181,25 +198,169 @@ void Vector<T>::traverse(void (*visit)(T &))
     }
 }
 
-template<typename T>template<typename VST>//原宿类型，操作器
-void Vector<T>::traverse(VST& visit)
+template <typename T>
+template <typename VST> // 原宿类型，操作器
+void Vector<T>::traverse(VST &visit)
 {
-    for (int i = 0; i < size;i++)
+    for (int i = 0; i < size(); i++)
         visit(_elem[i]);
 }
 
-//唯一化
-template<typename T>int Vector<T>::uniquify(){
-//低效    // int oldSize = _size;
-    // int i;
-    // while(i<_size)
-    //     _elem[i - 1] == _elem[i] ? remove(i) : i++;
-    // return oldSize - _size;
-    Rank i = 0,j = 0;
-    while(++j<_size)
-        if(_elem[i]!=_elem[j])
+// 唯一化
+template <typename T>
+int Vector<T>::uniquify()
+{
+    // 低效    // int oldSize = _size;
+    //  int i;
+    //  while(i<_size)
+    //      _elem[i - 1] == _elem[i] ? remove(i) : i++;
+    //  return oldSize - _size;
+    Rank i = 0, j = 0;
+    while (++j < _size)
+        if (_elem[i] != _elem[j])
             _elem[++i] = _elem[j];
     _size = ++i;
     shrink();
     return j - i;
+}
+
+// 气泡排序
+template <typename T>
+void Vector<T>::bubbleSort(Rank lo, Rank hi)
+{
+    while (!bubble(lo, hi--))
+        ;
+}
+
+// 二路归并借口
+template <typename T>
+void merge(Rank lo, Rank mi, Rank hi)
+{
+    T *A = _elem + lo;
+    int lb = mi - lo;
+    T *B = new T[lb];
+    for (Rank i = 0; i < lb; B[i] = A[i++])
+        ;
+    int lc = hi - mi;
+    T *C = _elem + mi;
+    for (Rank i = 0, j = 0, k = 0; (j < lb) || (k < lc);)
+    {
+        if ((j < lb) && (!(k < lc) || B[j] = C[k]))
+            A[i++] = B[j++];
+        if ((k < lc) && (!(j < lb) || C[k] = B[j]))
+            A[i++] = C[k++];
+    }
+    delete[] B;
+}
+// 归并排序
+template <typename T>
+void Vector<T>::mergeSort(Rank lo, Rank hi)
+{
+    if (hi - lo < 2)
+        return;
+    int mi = (lo + hi) / 2;
+    mergeSort(lo, mi);
+    mergeSort(mi, hi);
+    merge(lo, mi, hi);
+}
+
+// vector模板类
+
+// 复数类
+
+using namespace std;
+
+class Complex
+{
+public:
+    double real, imag;
+    Complex(double r = 0, double i = 0) : real(r), imag(i) {}
+
+    double modulus() const
+    {
+        return sqrt(real * real + imag * imag);
+    }
+
+    bool operator<(const Complex &other) const
+    {
+        return modulus() < other.modulus() || (modulus() == other.modulus() && real < other.real);
+    }
+
+    // 重载 << 运算符以便打印 Complex 对象
+    friend ostream &operator<<(ostream &os, const Complex &c)
+    {
+        os << "(" << c.real << ", " << c.imag << "i)";
+        return os;
+    }
+};
+
+// 定义一个函数来打印复数
+void print(const Complex &c)
+{
+    cout << c << " ";
+}
+
+int main()
+{
+    srand(static_cast<unsigned int>(time(0))); // 随机数种子
+
+    // 创建无序向量
+    Vector<int> vec;
+    for (int i = 0; i < 10; ++i)
+    {
+        vec.insert(i, rand() % 10); // 插入随机数
+    }
+
+    std::cout << "Original Vector: ";
+    vec.traverse(print);
+    std::cout << std::endl;
+
+    // 置乱
+    vec.unsort();
+    std::cout << "Shuffled Vector: ";
+    vec.traverse(print);
+    std::cout << std::endl;
+
+    // 查找
+    int toFind = 5;
+    Rank index = vec.find(toFind);
+    std::cout << "Find " << toFind << " at index: " << index << std::endl;
+
+    // 插入
+    vec.insert(3, 20);
+    std::cout << "After Insertion: ";
+    vec.traverse(print);
+    std::cout << std::endl;
+
+    // 删除
+    vec.remove(2);
+    std::cout << "After Deletion: ";
+    vec.traverse(print);
+    std::cout << std::endl;
+
+    // 唯一化
+    vec.uniquify();
+    std::cout << "After Uniquification: ";
+    vec.traverse(print);
+    std::cout << std::endl;
+
+    vec.sort();
+
+    // 测试不同的排序算法
+
+    clock_t start, end;
+
+    // 冒泡排序
+    start = clock();
+    vec.bubbleSort(0, vec.size());
+    end = clock();
+    std::cout << "Bubble Sort Time: " << static_cast<double>(end - start) / CLOCKS_PER_SEC << " seconds." << std::endl;
+
+    // 归并排序
+    start = clock();
+    vec.mergeSort(0, vec.size());
+    end = clock();
+    std::cout << "Merge Sort Time: " << static_cast<double>(end - start) / CLOCKS_PER_SEC << " seconds." << std::endl;
+
+    return 0;
 }
